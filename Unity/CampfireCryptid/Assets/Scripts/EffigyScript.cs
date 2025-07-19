476e0759887d9441cb7e7787c9b27126665f405a
Gavin Schmidt <gavinschmidt47@yahoo.com>
2025-07-19 17:34:55 -0400

    Effigy game working

--- Unity/CampfireCryptid/Assets/Scripts/EffigyScript.cs
+++ Unity/CampfireCryptid/Assets/Scripts/EffigyScript.cs
@@ -1,65 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EffigyScript : MonoBehaviour
 {
     public GameObject effigyPrefab; // Prefab for the effigies
     public int numberOfEffigies; // Number of effigies in the scene
     public int maxNumberOfEffigies = 10; // Maximum number of effigies allowed
-    public int minTimeBetween = 5; // Time in seconds between each spawn
-    public int maxTimeBetween = 15; // Time in seconds between each spawn
-    public int effigyCurLoc; // Current location of the effigy
+    public GlobalData globalData; // Reference to the GlobalData scriptable object
 
-    
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(EffigySpawner());
         List<Vector3> effigyPositions = new List<Vector3>(); // List to store the positions of the effigies
-        effigyPositions.Add(new Vector3(4.41400003,1.75999999,-1.58000004));
-        effigyPositions.Add(new Vector3(-2.82800007,1.18200004,-4.33199978));
-        effigyPositions.Add(new Vector3(-2.82800007,2.44799995,-4.33199978));
-        effigyPositions.Add(new Vector3(-2.82800007,6.04799986,-4.33199978));
-        effigyPositions.Add(new Vector3(0.148000002,1.21899998,-1.78699994));
-        effigyPositions.Add(new Vector3(0.148000002,2.86299992,-1.78699994));
-        effigyPositions.Add(new Vector3(0.148000002,4.57000017,-1.78699994));
-        effigyPositions.Add(new Vector3(-2.1400001,0.860000014,-1.26999998));
-        effigyPositions.Add(new Vector3(-2.18000007,2.68000007,-0.959999979));
-        effigyPositions.Add(new Vector3(-1.95099998,5.03999996,-1.27999997));
-        effigyPositions.Add(new Vector3(-11.257,1.18200004,-1.30299997));
-        effigyPositions.Add(new Vector3(-11.1409998,3.50399995,-1.48800004));
-        effigyPositions.Add(new Vector3(-11.1730003,6.1420002,-1.28299999));
-        effigyPositions.Add(new Vector3(-11.1730003,8.86999989,-1.28299999));
-        effigyPositions.Add(new Vector3(-8.98999977,6.67999983,2.68000007));
-        effigyPositions.Add(new Vector3(-8.98999977,2.77999997,2.68000007));
-        effigyPositions.Add(new Vector3(-5.2420001,0.845000029,2.96799994));
-        effigyPositions.Add(new Vector3(-5.2420001,3.49300003,2.96799994));
-        effigyPositions.Add(new Vector3(-5.2420001,8.43000031,2.96799994));
-        effigyPositions.Add(new Vector3(0.714999974,2.55999994,2.59599996));
-        effigyPositions.Add(new Vector3(0.714999974,4.61000013,2.59599996));
-        effigyPositions.Add(new Vector3(-0.617999971,4.61000013,4.32499981));
-        effigyPositions.Add(new Vector3(-0.617999971,1.31099999,4.32499981));
-        effigyPositions.Add(new Vector3(-0.617999971,3.38199997,4.32499981));
-        effigyPositions.Add(new Vector3(1.91400003,3.38199997,4.1079998));
-        effigyPositions.Add(new Vector3(1.91400003,1.39400005,4.1079998));
-        effigyPositions.Add(new Vector3(4.95300007,3.023,3.17000008));
-        effigyPositions.Add(new Vector3(4.95300007,1.63,3.17000008));
+        effigyPositions.Add(new Vector3(4.41400003f, 1.75999999f, -1.58000004f));
+        effigyPositions.Add(new Vector3(-2.82800007f, 1.18200004f, -4.33199978f));
+        effigyPositions.Add(new Vector3(-2.82800007f, 2.44799995f, -4.33199978f));
+        effigyPositions.Add(new Vector3(-2.82800007f, 6.04799986f, -4.33199978f));
+        effigyPositions.Add(new Vector3(0.148000002f, 1.21899998f, -1.78699994f));
+        effigyPositions.Add(new Vector3(0.148000002f, 2.86299992f, -1.78699994f));
+        effigyPositions.Add(new Vector3(0.148000002f, 4.57000017f, -1.78699994f));
+        effigyPositions.Add(new Vector3(-2.1400001f, 0.860000014f, -1.26999998f));
+        effigyPositions.Add(new Vector3(-2.18000007f, 2.68000007f, -0.959999979f));
+        effigyPositions.Add(new Vector3(-1.95099998f, 5.03999996f, -1.27999997f));
+        effigyPositions.Add(new Vector3(-11.257f, 1.18200004f, -1.30299997f));
+        effigyPositions.Add(new Vector3(-11.1409998f, 3.50399995f, -1.48800004f));
+        effigyPositions.Add(new Vector3(-11.1730003f, 6.1420002f, -1.28299999f));
+        effigyPositions.Add(new Vector3(-11.1730003f, 8.86999989f, -1.28299999f));
+        effigyPositions.Add(new Vector3(-8.98999977f, 6.67999983f, 2.68000007f));
+        effigyPositions.Add(new Vector3(-8.98999977f, 2.77999997f, 2.68000007f));
+        effigyPositions.Add(new Vector3(-5.2420001f, 0.845000029f, 2.96799994f));
+        effigyPositions.Add(new Vector3(-5.2420001f, 3.49300003f, 2.96799994f));
+        effigyPositions.Add(new Vector3(-5.2420001f, 8.43000031f, 2.96799994f));
+        effigyPositions.Add(new Vector3(0.714999974f, 2.55999994f, 2.59599996f));
+        effigyPositions.Add(new Vector3(0.714999974f, 4.61000013f, 2.59599996f));
+        effigyPositions.Add(new Vector3(-0.617999971f, 4.61000013f, 4.32499981f));
+        effigyPositions.Add(new Vector3(-0.617999971f, 1.31099999f, 4.32499981f));
+        effigyPositions.Add(new Vector3(-0.617999971f, 3.38199997f, 4.32499981f));
+        effigyPositions.Add(new Vector3(1.91400003f, 3.38199997f, 4.1079998f));
+        effigyPositions.Add(new Vector3(1.91400003f, 1.39400005f, 4.1079998f));
+        effigyPositions.Add(new Vector3(4.95300007f, 3.023f, 3.17000008f));
+        effigyPositions.Add(new Vector3(4.95300007f, 1.63f, 3.17000008f));
+        
+        numberOfEffigies = Random.Range(5, maxNumberOfEffigies + 1); // Randomly determine the number of effigies to spawn
+
+        for (int i = 0; i < numberOfEffigies; i++)
+        {
+            Debug.Log("Spawning effigy " + (i + 1) + " of " + numberOfEffigies);
+            int j = Random.Range(0, effigyPositions.Count); // Randomly select a position from the list
+            if (!effigyPositions[j].Equals(Vector3.zero)) // Check if the position is not zero
+            {
+                // Instantiate the effigy at the specified position
+                Instantiate(effigyPrefab, effigyPositions[j], Quaternion.identity);
+                effigyPositions[j] = Vector3.zero; // Mark this position as used
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    IEnumerator EffigySpawner()
-    {
-        while(numberOfEffigies < 10)
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame )
         {
-            
-			Instantiate(prefab, effigyCurLoc, Quaternion.identity);
-            yeild return new WaitForSeconds(Random.Range(minTimeBetween, maxTimeBetween));
-            numberOfEffigies++;
+            // Get the mouse position
+            Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+            // Create a ray from the camera to the mouse position
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                // Debug log to see what was hit
+                Debug.Log("Hit: " + hit.collider.name);
+                // Check if the hit object is an effigy
+                if (hit.collider.CompareTag("Effigy"))
+                {
+                    // Destroy the effigy
+                    Destroy(hit.collider.gameObject);
+                    Debug.Log("Effigy destroyed!");
+                    // Update global data
+                    //globalData.minigameProgress[1]++; // Increment Effigies minigame progress
+                }
+            }
         }
     }
 }
