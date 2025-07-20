a0dd204265cd68748cdc44f8ed97f478976b0936
Eric Nortmann <93231676+Marsh14@users.noreply.github.com>
2025-07-20 14:02:58 -0400

    timer added

---
+++ Unity/CampfireCryptid/Assets/Scripts/ShowTimer.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ShowTimer : MonoBehaviour
+{
+    // This script is used to show the timer in the UI
+    // It will display the time left before night
+    public GlobalData globalData; 
+    public TMPro.TextMeshProUGUI timerTextTMP; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        globalData.TimeSetDown(Time.deltaTime);
+
+        timerTextTMP.text = $"Time Left: {globalData.secondsLeftBeforeNight}";
+        Debug.Log($"secondsLeftBeforeNight: {globalData.secondsLeftBeforeNight}");
+    }
+
+
+
+}
+
